1d1036df0ef32a9a7ccbd32d6e537a936c7801e2
krahul2024 <culerrahul2024@gmail.com>
2026-02-02 03:11:24 +0530

    moved a bit of junk from places

--- medium/services/auth/src/main/java/com/auth/App.java
+++ medium/services/auth/src/main/java/com/auth/App.java
@@ -1,55 +1,9 @@
 package com.auth;
 
-import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Properties;
+import com.auth.others.Others;
 
 public class App {
     public static void main(String[] args) {
-    }
-
-    static void connect() {
-        Properties props = new Properties();
-
-        try(InputStream input = App.class.getClassLoader().getResourceAsStream("db.properties")) {
-            if (input == null) {
-                System.out.println("db.properties file not found!");
-                return;
-            }
-            props.load(input);
-
-
-            String url = props.getProperty("db.url");
-            String user = props.getProperty("db.username");
-            String password = props.getProperty("db.password");
-
-            System.out.println("url = " + url + "\nuser = " + user + "\npassword = " + password);
-
-            Connection conn = DriverManager.getConnection(url, user, password);
-            if (conn == null) {
-                System.out.println("unable to connect to the db!");
-                return;
-            }
-
-            System.out.println("Connected to the db");
-            simpleCheck(url, user, password);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    static void simpleCheck(String url, String user, String password) {
-        try (Connection conn = DriverManager.getConnection(url, user, password)) {
-            var stmt = conn.createStatement();
-            var rs = stmt.executeQuery("SELECT 1");
-            if (rs.next()) {
-                System.out.println("Query successful: " + rs.getInt(1));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        Others.testDBConn();
     }
 }
